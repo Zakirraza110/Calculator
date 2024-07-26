197aedcc38cf1a9c5c674ec2c1f56f62fbadc2b1
Zakir <zaakiraza110@gmail.com>
2024-07-25 23:23:30 -0700

    Arrow functions

--- file.js
+++ file.js
@@ -11,85 +11,85 @@ function clearscreen(){
     a.value="";
 }
 
-function cross(){
+const cross=()=>{
     const myarr=Array.from(a.value);
     console.log(myarr);
     myarr.pop();
     a.value=myarr.join("")
 }
 
-function funplus(){
+const funplus=()=>{
     a.value=a.value+'+';
 }
 
-function funminus(){
+const funminus=()=>{
     a.value=a.value+'-';
 }
 
-function funmulti(){
+const funmulti=()=>{
     a.value=a.value+'x';
 }
 
-function fundvide(){
+const fundvide=()=>{
     a.value=a.value+'/';
 }
 
-function fundot(){
+const fundot=()=>{
     a.value=a.value+'.';
 }
 
-function funmod(){
+const funmod=()=>{
     a.value=a.value+'%';
 }
 
 let zero=document.getElementById('zero');
-function fun(){
+const fun=()=>{
     a.value=a.value+'0';
 }
 
 
 let one=document.getElementById('one');
-function fun1(){
+const fun1=()=>{
     a.value=a.value+'1';
 }
 
 let two=document.getElementById('two');
-function fun2(){
+const fun2=()=>{
     a.value=a.value+'2';
 }
 
 let three=document.getElementById('three');
-function fun3(){
+const fun3=()=>{
     a.value=a.value+'3';
 }
 
 let four=document.getElementById('four');
-function fun4(){
+const fun4=()=>{
     a.value=a.value+'4';
 }
-function fun5(){
+const fun5=()=>{
     let five=document.getElementById('five');
     a.value=a.value+'5';
 }
-function fun6(){
+const fun6=()=>{
     let six=document.getElementById('six');
     a.value=a.value+'6';
 }
-function fun7(){
+const fun7=()=>{
     let seven=document.getElementById('seven');
     a.value=a.value+'7';
 }
-function fun8(){
+const fun8=()=>{
     let eight=document.getElementById('eight');
     a.value=a.value+'8';
 }
-function fun9(){
+const fun9=()=>{
     let nine=document.getElementById('nine');
     a.value=a.value+'9';
 }
 
 
-function funequal(){
+const funeal=()=>{
     let arr=Array.from(a.value);
     console.log(arr);
 }
